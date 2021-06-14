253193a86546fb0561cebc9310e8daa436cc08d3
Bhumit <patelbhumit83@gmail.com>
2021-06-14 11:54:02 -0700

    PostContruct and PreDestroy

--- Springfy/src/main/java/org/example/HelloWorld.java
+++ Springfy/src/main/java/org/example/HelloWorld.java
@@ -3,6 +3,9 @@ package org.example;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 @Scope("prototype")  // For getting new object everytime...
 public class HelloWorld {
@@ -14,6 +17,18 @@ public class HelloWorld {
     public void hello()
     {
         System.out.println("Inside Helloworld Class");
+    }
+
+    @PostConstruct
+    public void afterConstructor()
+    {
+        System.out.println("After Constructor");
+    }
+
+    @PreDestroy
+    public void preDestroy()
+    {
+        System.out.println("Before Destroy");
     }
 
 }
